1011ca3938e820187357b9b98fa2da0f70dcf33b
Jace Benson <jace.benson@gmail.com>
2020-12-08 00:50:20 -0600

    removed fontawesome, removed lots of jquery plugins

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,12 +1,12 @@
 module.exports = 
 {
-    twitter: "jacebenson",
+    twitter: "https://twitter.com/jacebenson",
     github: "https://github.com/jace-ty",
     linkedin: "https://linkedin.com/in/jace-ty",
     baseURL: "https://jace-ty.netlify.app",
-    title: "Jace-ty Blog",
+    title: "Jace-ty",
     
-    //patreon: "https://www.patreon.com/bePatron?u=23597006",//if uncommented adds a link to header and footer
+    patreon: "https://www.patreon.com/bePatron?u=23597006",//if uncommented adds a link to header and footer
     subtitle: "I am writing about my experiences as a ServiceNow Developer.",
     author: "Your Name",//used all over
     email: "youremailforrss@example.com",//used specificly for rss feed
